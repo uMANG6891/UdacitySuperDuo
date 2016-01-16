3eabfb8700ee98afa67ddc100a87aa06ddd3b217
Umang Pandya <umang6891@gmail.com>
2016-01-16 19:39:31 +0530

    - Add Toolbar in preference activity - Change color scheme - Add README and LICENCE

---
+++ README.md
@@ -0,0 +1,11 @@
+# Alexandria - Super Duo (Udacity Nanodegree Project - P3)
+
+I made this project while my Nanodegree program with Udacity. This app uses most of the Material guidelines.
+Marked Exceeds Specifications by Udacity Reviewers.
+
+## Features of Alexandria
+
+- List of saved books
+- Scan book barcode to add book
+- View and share book description
+- Delete book
--- alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
@@ -2,6 +2,10 @@ package it.jaschke.alexandria;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by saj on 27/01/15.
@@ -12,5 +16,15 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.preference_toolbar, root, false);
+        root.addView(bar, 0);
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
